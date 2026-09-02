53bdcbf754f4d0d28407d087551e0a81c440a100
Hjxnneveg <hjxnneveg@gmail.com>
2026-09-01 23:09:45 -0400

    move debugging macros to logging

---
+++ test/test_logging.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025-2026 Joshua C Marshall
+
+#include <core/logging.hpp>
+
+#include <core/test/tests.hpp>
+
+namespace hjx {
+
+// todo
+
+}
+
+int main(int, const char *argv[]) {
+    try {
+        hjx::test::harness::exec("core:logging", argv);
+        return 0;
+    }
+    catch (const char *s) {
+        if (strcmp(s, FAIL_MSG))
+            std::cout << "unexpected exception " << s << std::endl;
+    }
+    catch (...) {
+        std::cout << "unknown exception" << std::endl;
+    }
+
+    return 1;
+}
--- util.hpp
+++ util.hpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2025 Joshua C Marshall
+// Copyright (c) 2025-2026 Joshua C Marshall
 
 #pragma once
 
@@ -23,80 +23,6 @@
 
 // fixme - doesn't work for STL temporaries
 #define TO_STRING(x) (static_cast<std::ostringstream&&>(std::ostringstream() << x).str())
-
-#define ONCE(...) BRIEFLY(1, __VA_ARGS__)
-
-#define BRIEFLY(N, ...)                                                         \
-    do {                                                                        \
-        static std::mutex *MKVARNAME(mut) = NEW_STATIC_OBJECT(std::mutex);      \
-        static unsigned MKVARNAME(doit) = N;                                    \
-                                                                                \
-        std::unique_lock MKVARNAME(lock)(*MKVARNAME(mut));                      \
-        if (MKVARNAME(doit)) {                                                  \
-            MKVARNAME(doit)--;                                                  \
-            MKVARNAME(lock).unlock();                                           \
-            __VA_ARGS__;                                                        \
-        }                                                                       \
-    } while (false)
-
-#define ONCEPER(t, ...)                                                         \
-    do {                                                                        \
-        static std::mutex *MKVARNAME(mut) = NEW_STATIC_OBJECT(std::mutex);      \
-        static std::chrono::steady_clock::time_point MKVARNAME(last);           \
-                                                                                \
-        std::unique_lock MKVARNAME(lock)(*MKVARNAME(mut));                      \
-        auto MKVARNAME(now) = std::chrono::steady_clock::now();                 \
-        if (MKVARNAME(now) > MKVARNAME(last) + (t)) {                           \
-            MKVARNAME(last) = MKVARNAME(now);                                   \
-            MKVARNAME(lock).unlock();                                           \
-            __VA_ARGS__;                                                        \
-        }                                                                       \
-    } while (false)
-
-#if 0
-#define WATCH(...)                                                              \
-    WATCH_F((__VA_ARGS__), [](const auto &v) {                                  \
-        DUMPI(#__VA_ARGS__ << ": " << v);                                       \ fixme
-    })
-#endif
-
-#define WATCH_F(x, f)                                                           \
-    do {                                                                        \
-        static std::mutex *MKVARNAME(mut) = NEW_STATIC_OBJECT(std::mutex);      \
-        static bool MKVARNAME(first) = true;                                    \
-        static std::remove_reference_t<decltype(x)> MKVARNAME(cur){};           \
-                                                                                \
-        std::unique_lock MKVARNAME(lock)(*MKVARNAME(mut));                      \
-        decltype(x) MKVARNAME(val) = x;                                         \
-        if (MKVARNAME(first) || MKVARNAME(val) != MKVARNAME(cur)) {             \
-            (f)(MKVARNAME(val));                                                \
-            MKVARNAME(first) = false;                                           \
-            MKVARNAME(cur) = MKVARNAME(val);                                    \
-        }                                                                       \
-    } while (false)
-
-#define WATCH_RANGE(x, f)                                                               \
-    do {                                                                                \
-        static std::mutex *MKVARNAME(mut) = NEW_STATIC_OBJECT(std::mutex);              \
-        static decltype(x) MKVARNAME(min) = std::numeric_limits<decltype(x)>::max();    \
-        static decltype(x) MKVARNAME(max) = std::numeric_limits<decltype(x)>::lowest(); \
-                                                                                        \
-        std::unique_lock MKVARNAME(lock)(*MKVARNAME(mut));                              \
-        decltype(x) MKVARNAME(val) = x;                                                 \
-        bool MKVARNAME(changed) = false;                                                \
-                                                                                        \
-        if (MKVARNAME(val) < MKVARNAME(min)) {                                          \
-            MKVARNAME(min) = MKVARNAME(val);                                            \
-            MKVARNAME(changed) = true;                                                  \
-        }                                                                               \
-                                                                                        \
-        if (MKVARNAME(val) > MKVARNAME(max)) {                                          \
-            MKVARNAME(max) = MKVARNAME(val);                                            \
-            MKVARNAME(changed) = true;                                                  \
-        }                                                                               \
-                                                                                        \
-        if (MKVARNAME(changed)) (f)(MKVARNAME(min), MKVARNAME(max));                    \
-    } while (false)
 
 namespace hjx {
 
